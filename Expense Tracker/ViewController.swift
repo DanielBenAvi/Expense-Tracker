45f05aa6f5acab915a3475b2a967688f71e04354
Daniel Ben Avi <daniel.ben.avi10@gmail.com>
2024-05-31 16:47:35 +0300

    Initial Commit

---
+++ Expense Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expense Tracker
+//
+//  Created by דניאל בן אבי on 31/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
